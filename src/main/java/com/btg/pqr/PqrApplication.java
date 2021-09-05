9118cbca9776d8d6124400b7485873f5ce669b55
jfar007 <46226351+jfar007@users.noreply.github.com>
2021-09-04 21:46:18 -0500

    Init Repository

---
+++ src/main/java/com/btg/pqr/PqrApplication.java
@@ -0,0 +1,13 @@
+package com.btg.pqr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PqrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PqrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/btg/pqr/PqrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.btg.pqr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PqrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
